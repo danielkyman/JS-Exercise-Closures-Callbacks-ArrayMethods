035b829316cda3f1843e72644f21aab43b1dacd2
unknown <danny.kyman@gmail.com>
2020-01-28 16:49:57 -0800

    Completed all 25 questions

--- index.js
+++ index.js
@@ -236,8 +236,10 @@ function getRunnersByTShirtSize(runners, tShirtSize) {
  * @param runners array of runners like the one inside the /data/runners.js file.
  * @returns a number which is the sum of the donations by all runners.
 */
-function tallyUpDonations(/* CODE HERE */) {
-  /* CODE HERE */
+function tallyUpDonations(runners) {
+  const totalDonation = runners.reduce(function(accumulator, item){
+    return accumulator + item.donation;
+  }, 0);return totalDonation;
 }
 
 /////////////// CLOSURES ///////////////
@@ -258,11 +260,10 @@ function tallyUpDonations(/* CODE HERE */) {
 */
 function counterMaker() {
   // BROKEN CODE STARTS
-  const count = 0;
-  function counter() {
-    ++count
+  let count = 0;
+  return () => {
+    return count ++;
   }
-  // BROKEN CODE ENDS
 }
 
 /**
@@ -285,8 +286,14 @@ function counterMaker() {
  * counter() // should return 0
  * etc
 */
-function counterMakerWithLimit(/* CODE HERE */) {
-  /* CODE HERE */
+function counterMakerWithLimit(maxValue) {
+  let count = 0;
+  return function(){
+    if(count > maxValue){
+      count = 0;
+    }
+      return count++;  
+  }
 }
 
 /////////////// END OF CHALLENGE ///////////////
